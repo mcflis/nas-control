4825e942d8ca74deee946433efc1e1952050f6c4
Maximilian Flis <maximilian.flis@gmail.com>
2021-08-15 16:41:57 +0200

    update ips and netmasks to match new network settings on router

--- scripts/ipmi-tool.sh
+++ scripts/ipmi-tool.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-ipmitool -I lanplus -H 192.168.178.5 -U ADMIN -P "$(cat ~/.credentials/ipmi)" "$@"
+ipmitool -I lanplus -H 10.7.1.11 -U ADMIN -P "$(cat ~/.credentials/ipmi)" "$@"
